50fbbf2af5ea4977ada15170fe124f493c59b700
Laurence Ivanhoe Ancheta <eh202200036@wmsu.edu.ph>
2026-03-15 13:08:03 +0800

    Add files via upload

---
+++ Ancheta_CCAss2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int student, sub;
+
+    cout << "Input the number of students: ";
+    cin >> student;
+
+    cout << "Input the number of subjects: ";
+    cin >> sub;
+
+    vector<vector<double>> grades(student, vector<double>(sub));
+
+    cout << "\nEnter the Grades:\n";
+
+    for (int i = 0; i < student; i++) {
+        cout << "Student " << i + 1 << "\n";
+        for (int j = 0; j < sub; j++) {
+            cout << "Subject " << j + 1 << ": ";
+            cin >> grades[i][j];
+        }
+    }
+
+    double highest = grades[0][0];
+
+    cout << "\nStudent Grades:\n";
+
+    cout << "            ";
+    for (int j = 0; j < sub; j++) {
+        cout << "Sub" << j + 1 << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < student; i++) {
+        double sum = 0;
+
+        cout << "Student " << i + 1 << ":   ";
+
+        for (int j = 0; j < sub; j++) {
+            cout << grades[i][j] << "    ";
+            sum += grades[i][j];
+
+            if (grades[i][j] > highest)
+                highest = grades[i][j];
+        }
+
+        double avg = sum / sub;
+        cout << "Average: " << avg << endl;
+    }
+
+    cout << "\nHighest Grade: " << highest << endl;
+
+    return 0;
+}
